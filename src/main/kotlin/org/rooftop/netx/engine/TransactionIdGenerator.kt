9b3dbf9c173da23555ceae77111ea186a8030e56
devxb <dlwnsdud205@naver.com>
2024-02-02 19:27:22 +0900

    fix: TransactionIdGenerator에서 존재하지 않는 클래스 구현하고 있는 버그 수정

--- src/main/kotlin/org/rooftop/netx/engine/TransactionIdGenerator.kt
+++ src/main/kotlin/org/rooftop/netx/engine/TransactionIdGenerator.kt
@@ -2,9 +2,9 @@ package org.rooftop.netx.engine
 
 import com.github.f4b6a3.tsid.TsidFactory
 
-class TransactionIdGenerator : TransactionIdGenerator {
+class TransactionIdGenerator {
 
-    override fun generate(): String = tsidFactory.create().toLong().toString()
+    fun generate(): String = tsidFactory.create().toLong().toString()
 
     private companion object {
         private val tsidFactory = TsidFactory.newInstance256(110)
