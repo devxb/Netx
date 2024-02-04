583c92d028cac4f13bb88aa83b16ec4211522403
devxb <dlwnsdud205@naver.com>
2024-02-04 10:54:06 +0900

    test: RedisStreamTransactionManager의 Test를 작성한다

---
+++ src/test/kotlin/org/rooftop/netx/redis/RedisContainer.kt
@@ -0,0 +1,20 @@
+package org.rooftop.netx.redis
+
+import org.springframework.boot.test.context.TestConfiguration
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.utility.DockerImageName
+
+@TestConfiguration
+class RedisContainer {
+    init {
+        val redis: GenericContainer<*> = GenericContainer(DockerImageName.parse("redis:7.2.3"))
+            .withExposedPorts(6379)
+
+        redis.start()
+
+        System.setProperty(
+            "netx.port",
+            redis.getMappedPort(6379).toString()
+        )
+    }
+}
