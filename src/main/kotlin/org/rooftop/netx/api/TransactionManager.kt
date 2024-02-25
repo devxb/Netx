c22892f7d2f092a7b73e86fef65ff84079616491
devxb <dlwnsdud205@naver.com>
2024-02-25 15:35:46 +0900

    feat: sync 호출을 지원한다

--- src/main/kotlin/org/rooftop/netx/api/TransactionManager.kt
+++ src/main/kotlin/org/rooftop/netx/api/TransactionManager.kt
@@ -6,12 +6,22 @@ interface TransactionManager {
 
     fun start(undo: String): Mono<String>
 
+    fun syncStart(undo: String): String
+
     fun join(transactionId: String, undo: String): Mono<String>
+
+    fun syncJoin(transactionId: String, undo: String): String
 
     fun exists(transactionId: String): Mono<String>
 
+    fun syncExists(transactionId: String): String
+
     fun commit(transactionId: String): Mono<String>
 
+    fun syncCommit(transactionId: String): String
+
     fun rollback(transactionId: String, cause: String): Mono<String>
+
+    fun syncRollback(transactionId: String, cause: String): String
 
 }
