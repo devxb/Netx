e9090728825be0cdfc827d0536e5aa1fbed4ba33
devxb <dlwnsdud205@naver.com>
2024-03-24 17:13:21 +0900

    feat: Orchestrator start with context

--- src/main/kotlin/org/rooftop/netx/api/Orchestrator.kt
+++ src/main/kotlin/org/rooftop/netx/api/Orchestrator.kt
@@ -8,7 +8,15 @@ interface Orchestrator<T : Any, V : Any> {
 
     fun transaction(timeoutMillis: Long, request: T): Mono<Result<V>>
 
+    fun transaction(request: T, context: MutableMap<String, Any>): Mono<Result<V>>
+
+    fun transaction(timeoutMillis: Long, request: T, context: MutableMap<String, Any>): Mono<Result<V>>
+
     fun transactionSync(request: T): Result<V>
 
     fun transactionSync(timeoutMillis: Long, request: T): Result<V>
+
+    fun transactionSync(request: T, context: MutableMap<String, Any>): Result<V>
+
+    fun transactionSync(timeoutMillis: Long, request: T, context: MutableMap<String, Any>): Result<V>
 }
