de3a1a2191dfa6ad265397a710c0c0d792786836
xb205 <62425964+devxb@users.noreply.github.com>
2024-06-22 15:14:37 +0900

    feat: Support graceful Shutdown AbstractSagaListener (#141)
    * refactor: abstract saga listener를 graceful하게 종료한다
    
    * build: version to 0.4.3 -> 0.4.4-beta-1

--- src/main/kotlin/org/rooftop/netx/engine/AbstractSagaListener.kt
+++ src/main/kotlin/org/rooftop/netx/engine/AbstractSagaListener.kt
@@ -1,8 +1,10 @@
 package org.rooftop.netx.engine
 
+import jakarta.annotation.PreDestroy
 import org.rooftop.netx.engine.core.Saga
 import org.rooftop.netx.engine.logging.info
 import org.rooftop.netx.engine.logging.warningOnError
+import reactor.core.Disposable
 import reactor.core.publisher.BufferOverflowStrategy
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
@@ -13,8 +15,11 @@ internal abstract class AbstractSagaListener(
     private val sagaDispatcher: AbstractSagaDispatcher,
 ) {
 
+    private lateinit var disposable: Disposable
+    private var isShutdown = false
+
     fun subscribeStream() {
-        receive()
+        disposable = receive()
             .publishOn(Schedulers.boundedElastic())
             .onBackpressureBuffer(backpressureSize, BufferOverflowStrategy.DROP_LATEST)
             .doOnNext {
@@ -33,7 +38,18 @@ internal abstract class AbstractSagaListener(
 
     private fun <T> Flux<T>.restartWhenTerminated(): Flux<T> {
         return this.doAfterTerminate {
-            subscribeStream()
+            if (isShutdown.not()) {
+                subscribeStream()
+            }
         }
     }
+
+    @PreDestroy
+    private fun shutdownGracefully() {
+        disposable.dispose()
+        shutdownCascade()
+        info("Shutdown SagaListenerSupporter gracefully")
+    }
+
+    protected abstract fun shutdownCascade()
 }
